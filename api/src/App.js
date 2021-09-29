047923768ec0e84a859708df984fb1244ac58047
ALi <ali.hassan@codilitysolutions.com>
2021-09-29 16:29:50 +0500

    final

--- api/src/App.js
+++ api/src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
 import SignUp from "./components/SignUp";
-
+import Home from "./components/Home";
+import {BrowserRouter,Switch,Route} from "react-router-dom";
 function App() {
   return (
-  <SignUp />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={SignUp} />
+        <Route exact path="/home" component={Home} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- api/src/components/SignUp.js
+++ api/src/components/SignUp.js
@@ -1,14 +1,30 @@
 import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const SignUp = () => {
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
+  const history = useHistory();
 
-  function signup(){
-    let items={name,password,email}
-    console.log(items)
+  async function signup() {
+    let items = { name, password, email };
+    console.log(items);
+
+    let result = await fetch("https://jsonplaceholder.typicode.com/posts/1", {
+      method: "GET",
+      // body: JSON.stringify(items),
+      headers: {
+        "Content-type": "application/json",
+        Accept: "application/json",
+      },
+    });
+
+    result = await result.json();
+    localStorage.setItem("user-info", JSON.stringify(result));
+    history.push("./Home.js");
   }
+
   return (
     <div className="col-sm-6 offset-sm-3">
       <h1>SignUp page!</h1>
@@ -17,15 +33,15 @@ const SignUp = () => {
         value={name}
         onChange={(e) => setName(e.target.value)}
         className="form-control"
-        placeholder="name"
+        placeholder="Name"
       />
       <br />
       <input
-        type="text"
+        type="password"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
         className="form-control"
-        placeholder="name"
+        placeholder="Password"
       />
       <br />
       <input
@@ -33,10 +49,12 @@ const SignUp = () => {
         value={email}
         onChange={(e) => setEmail(e.target.value)}
         className="form-control"
-        placeholder="name"
+        placeholder="Email"
       />
       <br />
-      <button  onClick={signup}className="btn btn-primary d-flex justify-content-center" >Sign Up</button>
+      <button onClick={signup} className="btn btn-primary">
+        Sign Up
+      </button>
     </div>
   );
 };
